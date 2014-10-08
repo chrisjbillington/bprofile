1c53ac44ee0760767fd27c7cde7c669f4dd19f6d
cbilling <cbilling@localhost>
2014-10-08 19:07:54 -0400

    Renamed `report_delay` to `report_interval`
    Modified report generation to always occur the first time it is requested,
    and only delay subsequent requests.
    
    Fixed bug in finding 'dot' executable on linux.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A wrapper around profile/cProfile, gprof2dot and dot,
 providing a simple context manager for profiling sections
 of Python code and producing visual graphs of profiling results.
 
-( 
+(
 [view on pypi](https://pypi.python.org/pypi/bprofile/);
 [view on Bitbucket](https://bitbucket.org/cbillington/bprofile)
 )
@@ -16,7 +16,7 @@ of Python code and producing visual graphs of profiling results.
    
 This package provides a single class:
 
-    BProfile(output_path, threshold_percent=2.5, report_delay=5)
+    BProfile(output_path, threshold_percent=2.5, report_interval=5)
     
 A profiling context manager. Outputs a .png graph made via profile/cProfile, gprof2dot
 and graphviz. graphviz is the only external dependency.
@@ -26,57 +26,60 @@ and graphviz. graphviz is the only external dependency.
 
 ```
 #!python
-profile = BProfile('output.png')
+profiler = BProfile('output.png')
 
-with profile:
+with profiler:
     do_some_stuff()
 
 do_some_stuff_that_wont_be_profiled()
 
-with profile:
+with profiler:
     do_some_more_stuff()
 ```
 
 
 ### Arguments:
 
-    - output_path:       the name of the .png file you would like to output. '.png'
-                         will be appended if not present
-                         
-    - threshold_percent: int or float for the minimum percentage of total cumulative
-                         time a call should have to be included in the output. 
+    - output_path:       The name of the .png report file you would like to output.
+                        '.png' will be appended if not present.
+
+    - threshold_percent: `int` or `float` for the minimum percentage of total cumulative
+                         time a call should have to be included in the output.
                          Defaults to 2.5.
-                         
-    - report_delay:      How frequently output files should be produced. This is to
+
+    - report_interval:   The minimum time in between output file generation. This is to
                          minimise overhead on your program, (even though this overhead
                          will only be incurred when no code is being profiled), while
-                         allowing you to have somewhat immedaite results of the profiling
+                         allowing you to have ongoing results of the profiling
                          while your code is still running. Defaults to 5 (seconds).
 
 
 ### Output generation
 
-The profiler will return immediately after the context manager, and will generate
-its `.png` output after a delay (`report_delay`, default is 5 seconds) in a separate thread.
-The delay is to allow the block to execute many times before report generation,
-rather than slowing your program down with generating output all the time. This means
-that if your profile block is running repeatedly, a new output file will be produced
-every `report_delay` seconds.
+The profiler will return immediately after the context manager, and will
+generate its `.png` report in a separate thread. If the same context
+manager is used multiple times output will be generated at most every
+`report_interval` seconds (default: 5). The delay is to allow blocks to
+execute many times in between reports, rather than slowing your
+program down with generating graphs all the time. This means that if your
+profile block is running repeatedly, a new report will be produced every
+`report_interval` seconds.
 
-Pending reports will be generated at interpreter shutdown if delay_time has not yet
-elapsed.
+Pending reports will be generated at interpreter shutdown.
 
-Note that even if `report_delay` is short, it will not interfere with the profiling
-results themselves, as a lock is acquired that will prevent profiled code from running
-at the same time as the output generation code. So the overhead produced by output
-generation does not affect the results of profiling - this overhead will only affect
-portions of your code that are not being profiled.
+Note that even if `report_interval` is short, reporting will not interfere with the
+profiling results themselves, as a lock is acquired that will prevent profiled
+code from running at the same time as the report generation code. So the
+overhead produced by report generation does not affect the results of
+profiling - this overhead will only affect portions of your code that are not
+being profiled.
 
 The lock is shared between instances, and so you can freely instantiate many
-Profile objects to profile different parts of your code (but give them different output
-filenames!).
-Profile objects are thread safe, so you can also pass the same Profile object to different
-parts of your code to profile them on the same graph.
+Profile objects to profile different parts of your code. Instances with the same
+output file name will share an underlying profile/cProfile profiler, and so their
+reports will be combined. Profile objects are thread safe, however, so a single instance
+can be shared as well anywhere in your program.
 
-Note that since only one profiler can be running at a time, two profiled pieces of code
-waiting on each other in any way will deadlock.
+Note that since only one profiler can be running at a time, two profiled
+pieces of code waiting on each other in any way will deadlock, regardless of
+whether they share an underlying profile/cProfile profiler or not.
--- bprofile/bprofile.py
+++ bprofile/bprofile.py
@@ -23,7 +23,7 @@ try:
     import cProfile as profile
 except ImportError:
     import profile
-    
+
 this_folder = os.path.dirname(os.path.realpath(__file__))
 gprof2dot = os.path.join(this_folder, 'gprof2dot.py')
 
@@ -41,68 +41,80 @@ def find_dot():
         else:
             raise OSError('dot.exe not found, please install graphviz')
     else:
-        if subprocess.call("type dot", shell=True, stdout=devnull, stderr=devnull):
+        if subprocess.call(['type', 'dot'], shell=True, stdout=devnull, stderr=devnull):
             raise OSError('\'dot\' not found, please install graphviz')
         return 'dot'
 
-        
+
 DOT_PATH = find_dot()
-        
-        
+
+
 class BProfile(object):
-    """A profiling context manager. Outputs .png graph made via profile/cProfile, gprof2dot
+    """A profiling context manager. Outputs a .png graph made via profile/cProfile, gprof2dot
         and graphviz. graphviz is the only external dependency.
-        
-        arguments:
-            - output_path:       the name of the .png file you would like to output. '.png'
-                                 will be appended if not present
-                                 
-            - threshold_percent: int or float for the minimum percentage of total cumulative
-                                 time a call should have to be included in the output. 
+
+
+        ### Example usage:
+
+        ```
+        #!python
+        profiler = BProfile('output.png')
+
+        with profiler:
+            do_some_stuff()
+
+        do_some_stuff_that_wont_be_profiled()
+
+        with profiler:
+            do_some_more_stuff()
+        ```
+
+
+        ### Arguments:
+
+            - output_path:       The name of the .png report file you would like to output.
+                                '.png' will be appended if not present.
+
+            - threshold_percent: `int` or `float` for the minimum percentage of total cumulative
+                                 time a call should have to be included in the output.
                                  Defaults to 2.5.
-                                 
-            - report_delay:      How frequently output files should be produced. This is to
+
+            - report_interval:   The minimum time in between output file generation. This is to
                                  minimise overhead on your program, (even though this overhead
                                  will only be incurred when no code is being profiled), while
-                                 allowing you to have somewhat immedaite results of the profiling
+                                 allowing you to have ongoing results of the profiling
                                  while your code is still running. Defaults to 5 (seconds).
-        
-        usage example:
-        
-            profiler = BProfile('output.png')
-            
-            with profiler:
-                do_some_stuff()
-            
-            do_some_stuff_that_wont_be_profiled()
-            
-            with profiler:
-                do_some_more_stuff()
-            
-        The profiler will return immediately after the context manager, and will generate
-        its .png output after a delay (report_delay, default is 5 seconds) in a separate thread.
-        The delay is to allow the block to execute many times before report generation,
-        rather than slowing your program down with generating output all the time. This means
-        that if your profile block is running repeatedly, a new output file will be produced
-        every <report_delay> seconds.
-        
-        Pending reports will be generated at interpreter shutdown if delay_time has not yet
-        elapsed.
-        
-        Note that even if report_delay is short, it will not interfere with the profiling
-        results themselves, as a lock is acquired that will prevent profiled code from running
-        at the same time as the output generation code. So the overhead produced by output
-        generation does not affect the results of profiling - this overhead will only affect
-        portions of your code that are not being profiled.
-        
+
+
+        ### Output generation
+
+        The profiler will return immediately after the context manager, and will
+        generate its `.png` report in a separate thread. If the same context
+        manager is used multiple times output will be generated at most every
+        `report_interval` seconds (default: 5). The delay is to allow blocks to
+        execute many times in between reports, rather than slowing your
+        program down with generating graphs all the time. This means that if your
+        profile block is running repeatedly, a new report will be produced every
+        `report_interval` seconds.
+
+        Pending reports will be generated at interpreter shutdown.
+
+        Note that even if `report_interval` is short, reporting will not interfere with the
+        profiling results themselves, as a lock is acquired that will prevent profiled
+        code from running at the same time as the report generation code. So the
+        overhead produced by report generation does not affect the results of
+        profiling - this overhead will only affect portions of your code that are not
+        being profiled.
+
         The lock is shared between instances, and so you can freely instantiate many
-        Profile objects to profile different parts of your code (but give them different output
-        filenames!).
-        Profile objects are threadsafe, so you can also pass the same Profile object to different
-        parts of your code to profile them on the same graph.
-        
-        Note that since only one profiler can be running at a time, two profiled pieces of code
-        waiting on each other in any way will cause a deadlock.
+        Profile objects to profile different parts of your code. Instances with the same
+        output file name will share an underlying profile/cProfile profiler, and so their
+        reports will be combined. Profile objects are thread safe, however, so a single instance
+        can be shared as well anywhere in your program.
+
+        Note that since only one profiler can be running at a time, two profiled
+        pieces of code waiting on each other in any way will deadlock, regardless of
+        whether they share an underlying profile/cProfile profiler or not.
     """
     
     class_lock = threading.Lock()
@@ -110,14 +122,14 @@ class BProfile(object):
     report_thread = None
     instances_requiring_reports = set()
     
-    def __init__(self, output_path, threshold_percent=2.5, report_delay=5):
+    def __init__(self, output_path, threshold_percent=2.5, report_interval=5):
         if not output_path.lower().endswith('.png'):
             output_path += '.png'
         self.output_path = output_path
         self.threshhold_percent = threshold_percent
         self.profiler = profile.Profile()
-        self.time_of_last_report = time.time()
-        self.report_delay = report_delay
+        self.report_interval = report_interval
+        self.time_of_last_report = time.time() - report_interval
         with self.class_lock:
             # only one reporting thread to be shared between instances:
             if self.report_thread is None:
@@ -167,7 +179,7 @@ class BProfile(object):
                     timeout = None
                     continue
                 for instance in cls.instances_requiring_reports.copy():
-                    next_report_time = instance.time_of_last_report + instance.report_delay
+                    next_report_time = instance.time_of_last_report + instance.report_interval
                     time_until_report =  next_report_time - time.time()
                     if time_until_report < 0:
                         instance.do_report()
@@ -181,7 +193,7 @@ class BProfile(object):
                 
 if __name__ == '__main__':
     # Test:
-    profile = BProfile('test.png')
+    profiler = BProfile('test.png')
     def foo():
         time.sleep(0.05)
     def bar():
@@ -190,7 +202,7 @@ if __name__ == '__main__':
     start_time = time.time()    
     for i in range(100):
         print(i)
-        with profile:
+        with profiler:
             time.sleep(0.1)
             foo()
             bar()
